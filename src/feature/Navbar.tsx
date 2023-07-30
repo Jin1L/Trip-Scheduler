932e5c745facaeeff6d799920903c1b6c0465250
Youngsun Yoo <youngsun123q@gmail.com>
2023-07-30 18:35:07 -0400

    changed web tab title

--- src/feature/Navbar.tsx
+++ src/feature/Navbar.tsx
@@ -3,9 +3,7 @@ import NavMenu from "./Navmenu";
 const Navbar = () => {
   return (
     <nav className="flex p-6 py-4 bg-gray-100 border-b-4 border-cyan-300 items-center justify-between space-x-6">
-      <h1 className=" font-semibold tracking-tighter text-gray-700 text-xl ">
-        Oua
-      </h1>
+      <h1 className=" font-semibold text-gray-700 text-xl ">Oua</h1>
       <NavMenu />
     </nav>
   );
