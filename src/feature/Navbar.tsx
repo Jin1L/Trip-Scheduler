ea70cf6a9301cd1d06fd8f642e7e5ded8e90f574
Youngsun Yoo <youngsun123q@gmail.com>
2023-07-30 18:33:40 -0400

    change website title

--- src/feature/Navbar.tsx
+++ src/feature/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex p-6 py-4 bg-gray-100 border-b-4 border-cyan-300 items-center justify-between space-x-6">
       <h1 className=" font-semibold tracking-tighter text-gray-700 text-xl ">
-        You & I
+        Oua
       </h1>
       <NavMenu />
     </nav>
